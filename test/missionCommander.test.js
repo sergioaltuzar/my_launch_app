e41202b18f55c1414b19b37629f20371c8e1efc5
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-04-19 14:27:51 -0500

    prueba con Sergio passed

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -10,7 +10,7 @@ const MissionCommander = require('./../app/missionCommander');
 
   describe("Unit Tests for Mission Commander Class", () => {
     test('1) Create a mission commander objet', () => {
-      const myMissionCommander = new MissionCommander("Carlo");
-      expect(myMissionCommander.name).toBe("Jorge");
+      const myMissionCommander = new MissionCommander("Sergio");
+      expect(myMissionCommander.name).toBe("Sergio");
     });
   })
